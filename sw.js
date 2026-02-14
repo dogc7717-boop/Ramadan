63a56dd20966ddfa30cc6d8553001014c6affd8c
dogc7717-boop <dogc7717@gmail.com>
2026-02-14 07:32:13 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,40 @@
-const cacheName = 'subha-v1';
+const cacheName = 'sameh-nady-pro-v2';
 const assets = [
   './',
   './index.html',
-  './manifest.json'
+  './style.css',
+  './script.js',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Cairo&display=swap',
+  'https://www.soundjay.com/buttons/sounds/button-16.mp3'
 ];
 
-// تثبيت الخدمة وتخزين الملفات
-self.addEventListener('install', evt => {
-  evt.waitUntil(
+// تثبيت الخدمة وحفظ الملفات في الذاكرة
+self.addEventListener('install', e => {
+  e.waitUntil(
     caches.open(cacheName).then(cache => {
-      cache.addAll(assets);
+      return cache.addAll(assets);
     })
   );
 });
 
-// تشغيل التطبيق حتى بدون إنترنت
-self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(rec => {
-      return rec || fetch(evt.request);
+// تشغيل التطبيق حتى لو مفيش إنترنت (Offline Mode)
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
+
+// تحديث الملفات لو عدلت الكود
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== cacheName)
+        .map(key => caches.delete(key))
+      );
     })
   );
 });
