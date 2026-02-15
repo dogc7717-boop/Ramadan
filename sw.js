37899bd1b149e2684c780fff46e1876d9f870fca
dogc7717-boop <dogc7717@gmail.com>
2026-02-15 14:59:25 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,50 @@
-const cacheName = 'iskar-pro-v7';
-const assets = ['./', './index.html', './style.css', './script.js', './manifest.json', './Egypt.mp3', './Egypt_1.mp3', './Egypt_2.mp3', './Egypt_3.mp3'];
+/**
+ * ISKAR - Service Worker v8
+ * لضمان عمل التطبيق بدون إنترنت وتحديث الأكواد فوراً
+ */
+
+const cacheName = 'iskar-pro-v8';
+
+// قائمة الملفات التي سيتم تخزينها ليعمل التطبيق بدون إنترنت
+const assets = [
+  './',
+  './index.html',
+  './script.js',
+  './manifest.json',
+  './icon.png',
+  './Egypt.mp3',
+  './Egypt_1.mp3',
+  './Egypt_2.mp3',
+  './Egypt_3.mp3'
+];
+
+// تثبيت الـ Service Worker وتخزين الملفات
 self.addEventListener('install', e => {
-  self.skipWaiting();
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+  self.skipWaiting(); // إجبار التحديث فوراً
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      console.log('Caching assets...');
+      return cache.addAll(assets);
+    })
+  );
 });
+
+// حذف الكاش القديم عند تفعيل الإصدار الجديد
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== cacheName).map(k => caches.delete(k)))));
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(k => k !== cacheName).map(k => caches.delete(k))
+      );
+    })
+  );
 });
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(res => res || fetch(e.request))));
+
+// استدعاء الملفات من الكاش لضمان السرعة والعمل بدون نت
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
