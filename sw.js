47bc957a47938af94cd3544dc1b1f0ed6862226b
dogc7717-boop <dogc7717@gmail.com>
2026-02-18 19:59:25 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,50 +1,7 @@
-/**
- * ISKAR - Service Worker v8
- * لضمان عمل التطبيق بدون إنترنت وتحديث الأكواد فوراً
- */
-
-const cacheName = 'iskar-pro-v8';
-
-// قائمة الملفات التي سيتم تخزينها ليعمل التطبيق بدون إنترنت
-const assets = [
-  './',
-  './index.html',
-  './script.js',
-  './manifest.json',
-  './icon.png',
-  './Egypt.mp3',
-  './Egypt_1.mp3',
-  './Egypt_2.mp3',
-  './Egypt_3.mp3'
-];
-
-// تثبيت الـ Service Worker وتخزين الملفات
+const CACHE = 'ramadan-v3';
 self.addEventListener('install', e => {
-  self.skipWaiting(); // إجبار التحديث فوراً
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      console.log('Caching assets...');
-      return cache.addAll(assets);
-    })
-  );
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html', './style.css', './script.js', './azkar.json'])));
 });
-
-// حذف الكاش القديم عند تفعيل الإصدار الجديد
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(k => k !== cacheName).map(k => caches.delete(k))
-      );
-    })
-  );
-});
-
-// استدعاء الملفات من الكاش لضمان السرعة والعمل بدون نت
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
