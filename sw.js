fb703f743c3fc31c9800c5157e840daf48a354df
dogc7717-boop <dogc7717@gmail.com>
2026-02-18 20:52:23 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,10 @@
-const CACHE = 'ramadan-v3';
+const cacheName = 'iskar-v11';
+const assets = ['./', './index.html', './script.js', './style.css', './manifest.json', './icon.png', './cover.jpg', './Egypt.mp3', './Egypt_1.mp3', './Egypt_2.mp3', './Egypt_3.mp3'];
+
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html', './style.css', './script.js', './azkar.json'])));
+    e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
+
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
